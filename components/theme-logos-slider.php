363efe731c2a525908cfc842268cf92ef5695772
dakup101 <dakup101@gmail.com>
2023-10-05 10:44:42 +0200

    Logos changes

--- components/theme-logos-slider.php
+++ components/theme-logos-slider.php
@@ -5,13 +5,15 @@ $query_args = array(
 );
 $logos_1 = array();
 $logos_2 = array();
+$logos_3 = array(); // Add a new array for the third slider
 $counter = 1;
 $events = new WP_Query($query_args);
 if ($events->have_posts()){
     while($events->have_posts()){
         $events->the_post();
-        if ($counter % 2 == 0) array_push($logos_1, array(get_the_id(), get_the_title()));
-        else array_push($logos_2, array(get_the_id(), get_the_title()));
+        if ($counter % 3 == 0) array_push($logos_1, array(get_the_id(), get_the_title()));
+        elseif ($counter % 3 == 1) array_push($logos_2, array(get_the_id(), get_the_title()));
+        else array_push($logos_3, array(get_the_id(), get_the_title()));
         $counter++;
     }
 }
@@ -41,7 +43,7 @@ if ($events->have_posts()){
 </div>
 <div class="logos-slider logos-slider-2"
      dir="rtl">
-    <div class=" swiper-wrapper">
+    <div class="swiper-wrapper">
         <?php foreach($logos_2 as $el): ?>
         <div class="swiper-slide">
             <div class="flex items-center justify-center">
@@ -52,6 +54,28 @@ if ($events->have_posts()){
         </div>
         <?php endforeach; ?>
         <?php foreach($logos_2 as $el): ?>
+        <div class="swiper-slide">
+            <div class="flex items-center justify-center">
+                <img src="<?php echo get_field("logo", $el[0]) ?>"
+                     alt="<?php echo $el[1] ?>"
+                     class="max-w-full w-full">
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+<div class="logos-slider logos-slider-3">
+    <div class="swiper-wrapper">
+        <?php foreach($logos_3 as $el): ?>
+        <div class="swiper-slide">
+            <div class="flex items-center justify-center">
+                <img src="<?php echo get_field("logo", $el[0]) ?>"
+                     alt="<?php echo $el[1] ?>"
+                     class="max-w-full w-full">
+            </div>
+        </div>
+        <?php endforeach; ?>
+        <?php foreach($logos_3 as $el): ?>
         <div class="swiper-slide">
             <div class="flex items-center justify-center">
                 <img src="<?php echo get_field("logo", $el[0]) ?>"
--- src/ts/functions/logos-slider.ts
+++ src/ts/functions/logos-slider.ts
@@ -3,66 +3,51 @@ import "swiper/css/bundle";
 
 export default function logosSlider() {
 	console.log("--- Logos Slider Loaded ---");
-	const sliderOne = new Swiper(".logos-slider-1", {
-		modules: [FreeMode, Autoplay, A11y],
-		loop: true,
-		spaceBetween: 0,
-		// grabCursor: true,
-		slidesPerView: 3,
-		a11y: {
-			enabled: false,
-		},
-		autoplay: {
-			delay: 1,
-			disableOnInteraction: true,
-		},
-		freeMode: {
-			enabled: true,
-			momentum: false,
-		},
-		speed: 5000,
-		breakpoints: {
-			768: {
-				slidesPerView: 3,
-				speed: 4000,
+
+	function initializeSlider(selector) {
+		return new Swiper(selector, {
+			modules: [FreeMode, Autoplay, A11y],
+			loop: true,
+			spaceBetween: 30,
+			slidesPerView: 4,
+			allowTouchMove: false,
+			a11y: {
+				enabled: false,
 			},
-			1040: {
-				slidesPerView: 4,
-				speed: 5000,
+			autoplay: {
+				delay: 1,
+				disableOnInteraction: false,
 			},
-		},
-	});
-	const sliderTwo = new Swiper(".logos-slider-2", {
-		modules: [FreeMode, Autoplay, A11y],
-		loop: true,
-		spaceBetween: 30,
-		// grabCursor: true,
-		slidesPerView: 3,
-		a11y: {
-			enabled: false,
-		},
-		autoplay: {
-			delay: 1,
-			disableOnInteraction: true,
-			// reverseDirection: true,
-		},
-		freeMode: {
-			enabled: true,
-			momentum: false,
-		},
-		speed: 5000,
-		breakpoints: {
-			468: {
-				slidesPerView: 2,
+			freeMode: {
+				enabled: true,
+				momentum: false,
 			},
-			768: {
-				slidesPerView: 3,
-				speed: 4000,
+			speed: 5500,
+			breakpoints: {
+				320: {
+					slidesPerView: 2,
+					speed: 3500,
+				},
+				468: {
+					slidesPerView: 2.25,
+				},
+				648: {
+					slidesPerView: 2.5,
+					speed: 4000,
+				},
+				768: {
+					slidesPerView: 3,
+					speed: 4500,
+				},
+				1040: {
+					slidesPerView: 4,
+					speed: 5500,
+				},
 			},
-			1040: {
-				slidesPerView: 4,
-				speed: 5000,
-			},
-		},
-	});
+		});
+	}
+
+	const sliderOne = initializeSlider(".logos-slider-1");
+	const sliderTwo = initializeSlider(".logos-slider-2");
+	const sliderThree = initializeSlider(".logos-slider-3");
 }
